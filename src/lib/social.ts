a2c6df0820570db7d0e49035e28e0ba89473e5a1
globalorbitshub-prog <globalorbitshub@gmail.com>
2026-07-29 10:39:22 +0200

    Create social.ts

---
+++ src/lib/social.ts
@@ -0,0 +1,3 @@
+export const WHATSAPP_URL =
+  "https://api.whatsapp.com/send/?phone=34601539395&text&type=phone_number&app_absent=0";
+export const INSTAGRAM_URL = "https://www.instagram.com/jmc.perfumes";
